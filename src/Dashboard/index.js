f4d3afbbcbc8922287bb6ad289e51b83f10ba593
Simon Hutchinson <simonhutc@gmail.com>
2020-12-01 21:16:11 +1100

    added spotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import Page from '../Shared/Page';
-import PriceGrid from '../Dashboard/PriceGrid';
+import Page from "../Shared/Page";
+import PriceGrid from "../Dashboard/PriceGrid";
+import CoinSpotlight from "./CoinSpotlight";
+import styled from 'styled-components';
 
-
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function () {
   return (
     <Page name="dashboard">
-    <PriceGrid />
+      <PriceGrid />
+      <ChartGrid>
+        <CoinSpotlight />
+        <div>Chart goes here</div>
+      </ChartGrid>
     </Page>
   );
 }
